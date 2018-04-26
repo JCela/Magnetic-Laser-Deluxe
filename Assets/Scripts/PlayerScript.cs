ef922c8d25230a96924183d526be2e9b9350f727
Andrew <aw3390@nyu.edu>
2018-04-26 12:05:26 -0400

    First Commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	Rigidbody2D myRigidbody;
+	public float moveSpeed = 1.0f;
+
+
+	void Awake() {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		float verticalVelocity = Input.GetAxis ("Vertical");
+		float horizontalVelocity = Input.GetAxis ("Horizontal");
+		Vector2 velocity = new Vector2 (horizontalVelocity, verticalVelocity);
+
+		myRigidbody.velocity += velocity * moveSpeed;
+	}
+}
